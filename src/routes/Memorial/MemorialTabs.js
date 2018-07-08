a5c80e50d24b8d605dbad4c5ced91c3fb70f0242
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-07-08 14:38:46 -0500

    Add settings menu (mock)

--- src/routes/Memorial/MemorialTabs.js
+++ src/routes/Memorial/MemorialTabs.js
@@ -9,56 +9,81 @@ import Tab from '@material-ui/core/Tab';
 import Divider from '@material-ui/core/Divider';
 import Zoom from '@material-ui/core/Zoom';
 import Slide from '@material-ui/core/Slide';
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
 
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
 import UnfoldLessIcon from '@material-ui/icons/UnfoldLess';
+import CloseIcon from '@material-ui/icons/Close';
 
-const MemorialTabs = (props) => (
-   <Slide in={props.secondaryAppHeaderVisible} unmountOnExit >
-      <Paper id="MemorialHeader" elevation={4}>
-         <div style={{display: 'flex', width: '100%'}}>
-            <MemorialAvatar
-               src={props.memorial.avatar}
-               name={props.memorial.nm}
-               style={{
-                  paddingTop: 16,
-                  paddingLeft: 16
-               }}
-            />
-            <IconButton
-               style={{
-                  marginTop: 8,
-                  marginLeft: 'auto',
-                  marginRight: 4,
-               }}
-            >
-               <SettingsIcon/>
-            </IconButton>
-            <IconButton
-               onClick={props.hideSecondaryAppHeader}
-               style={{
-                  marginTop: 8,
-                  marginRight: 4,
-               }}
-            >
-               <UnfoldLessIcon/>
-            </IconButton>
-         </div>
-         <Tabs
-            value={props.view}
-            onChange={props.handleTabChange}
-            indicatorColor="primary"
-            textColor="primary"
-            fullWidth
-         >
-            <Tab label="Shrine" value="S" />
-            <Tab label="Chronicle" value="C" />
-            <Tab label="Atlas" value="A" />
-         </Tabs>
-         <Divider />
-      </Paper>
-   </Slide>
-)
+class MemorialTabs extends React.Component {
+   state = {
+      showSettingsMenu: false
+   }
+   
+   handleSettingsMenuClick = () => {
+      this.setState({ showSettingsMenu: !this.state.showSettingsMenu })
+   }
+
+   render () {
+      return (
+         <Slide in={this.props.secondaryAppHeaderVisible} unmountOnExit >
+            <Paper id="MemorialHeader" elevation={4}>
+               <div style={{display: 'flex', width: '100%'}}>
+                  <MemorialAvatar
+                     src={this.props.memorial.avatar}
+                     name={this.props.memorial.nm}
+                     style={{
+                        paddingTop: 16,
+                        paddingLeft: 16
+                     }}
+                  />
+                  <IconButton
+                     onClick={this.handleSettingsMenuClick}
+                     style={{
+                        marginTop: 8,
+                        marginLeft: 'auto',
+                        marginRight: 4,
+                     }}
+                  >
+                     {this.state.showSettingsMenu ? <CloseIcon /> : <SettingsIcon/>}
+                  </IconButton>
+                  <IconButton
+                     onClick={this.props.hideSecondaryAppHeader}
+                     style={{
+                        marginTop: 8,
+                        marginRight: 4,
+                     }}
+                  >
+                     <UnfoldLessIcon/>
+                  </IconButton>
+               </div>
+               <Collapse in={this.state.showSettingsMenu} style={{margin: 8}}>
+                  <Typography variant="subheading">
+                     Memorial Settings
+                  </Typography>
+                  <Typography variant="body1">
+                     This will be a small settings menu, with a link to the
+                     full memorial settings page
+                  </Typography>
+               </Collapse>
+               <Tabs
+                  value={this.props.view}
+                  onChange={this.props.handleTabChange}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  fullWidth
+               >
+                  <Tab label="Shrine" value="S" />
+                  <Tab label="Chronicle" value="C" />
+                  <Tab label="Atlas" value="A" />
+               </Tabs>
+               <Divider />
+            </Paper>
+         </Slide>
+      )
+   }
+}
 
 export default connect('secondaryAppHeaderVisible', actions)(MemorialTabs);
