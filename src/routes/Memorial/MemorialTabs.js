5c3469341ccaac3c23329499d29c93917a35db39
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-07-18 15:45:14 -0500

    Sync with AppHeader
    Slides in and out of viewport in sync with AppHeader (via headroom.js).
    Uses a MutationObserver to track when AppHeader is toggled (watches css
    classes) and sets visible state accordingly.
    
    Uses some scroll hackery at the top of the document to smooth the
    transition, and eventually toggle 'position' from 'relative' to 'fixed'.
    It's grossly inefficient as the onscroll callback is not debounced or
    throttled, but cpu/ram spikes were minimal (on my laptop) so whatever.
    
    I tried some naive debounce/throttle code found on the interwebs, but
    they would spin the fans up if I scrolled alot. I guess sometimes simple
    is the best.
    
    The RIGHT way to do all of this would be to move some state and
    functions to the store and then merge the MemorialTabs (a secondary
    AppHeader, more or less) with the actual AppHeader, so that it's all
    under the control of the 'headroom.js' component.
    
    But what's the fun in that? If I had just refactored the code, I would
    never have gone down the rabit hole to learn about
    debouncing/throttling, requestAnimationFrame(), MutationObservers, and a
    whole bunch of other stuff about DOM events. And the hacky version works
    pretty well. I'd go as far to say I'm rather proud of it.

--- src/routes/Memorial/MemorialTabs.js
+++ src/routes/Memorial/MemorialTabs.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles'
 import { connect } from 'unistore/react';
 import { actions } from 'store';
 import Paper from '@material-ui/core/Paper';
@@ -7,7 +6,6 @@ import MemorialAvatar from 'components/MemorialAvatar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Divider from '@material-ui/core/Divider';
-import Zoom from '@material-ui/core/Zoom';
 import Slide from '@material-ui/core/Slide';
 import Collapse from '@material-ui/core/Collapse';
 import Typography from '@material-ui/core/Typography';
@@ -19,17 +17,91 @@ import CloseIcon from '@material-ui/icons/Close';
 
 class MemorialTabs extends React.Component {
    state = {
-      showSettingsMenu: false
+      showSettingsMenu: false,
+      visible: true,
+      positionToggled: false
    }
-   
+
+   componentDidMount () {
+      // this is quickly becoming a mountain of hacks.
+      // should REALLY just figure out how to put all the shit together 
+      // in one headroom component.
+      
+      // fixes issue of first card being obscure by the menu..
+      // could just put a margin on top of the list of chronicle cards
+      window.onscroll = () => {
+         if (window.pageYOffset < 56) {
+            if (this.state.positionToggled) {
+               this.setState({ positionToggled: false })
+            }
+         }
+      }
+      // latest idea is to use a MutationObserver to watch for class
+      // changes on headroom header. I will find a workaround!!
+      //
+      // THIS WORKS!! and works pretty well, i might add
+      
+      // also would like to have the chronicle add item fab synced to
+      // this too, so it hides when scrolling down. easiest will be to
+      // copy and paste this code, but the right way would be to refactor
+      // everything so it's all in the store, and the memorial tabs are 
+      // contained within the Headroom header.
+      //
+      // but realistically, i'll take on the technical debt and do it the
+      // hacky way for now until the experience is settled upon. move fast,
+      // break shit, write incomprehensible code, amirite? it sure is pretty
+      // though.
+
+      // observer to watch classes on AppBar
+      this.observer = new MutationObserver(observerCallback);
+      this.observer.observe(document.getElementById('AppHeader'), {
+         attributes: true,
+         atrributeFilter: ['class']
+      });
+      var that = this;
+      // when headroom is visible, make the secondary header visible
+      function observerCallback (mutations, observer) {
+         mutations.forEach((e) => {
+            // console.log(e.target.classList);
+            if (e.target.classList.contains('headroom--unpinned') && that.state.visible && window.pageYOffset > 126) {
+               that.setState({ visible: false })
+            }
+            if (e.target.classList.contains('headroom--pinned') && window.pageYOffset > 126) {
+               that.setState({ visible: true })
+               if(!that.state.positionToggled) {
+                  setTimeout(() => {that.setState({ positionToggled: true })}, 200);
+               }
+            }
+         })
+      }
+   }
+
+   componentWillUnmount () {
+      window.onscroll = false;
+      this.observer.disconnect();
+   }
+
    handleSettingsMenuClick = () => {
       this.setState({ showSettingsMenu: !this.state.showSettingsMenu })
    }
 
    render () {
       return (
-         <Slide in={this.props.secondaryAppHeaderVisible} unmountOnExit >
-            <Paper id="MemorialHeader" elevation={4}>
+         <Slide
+            in={
+               !this.state.positionToggled ? true :
+                  this.state.visible ? true : false
+            }
+         >
+            <Paper
+               id="MemorialHeader"
+               elevation={4}
+               style={{
+                  position: (this.state.positionToggled ? 'fixed' : 'relative'),
+                  zIndex: 1,
+                  width: '100%',
+               }}
+            >
                <div style={{display: 'flex', width: '100%'}}>
                   <MemorialAvatar
                      src={this.props.memorial.avatar}
@@ -49,15 +121,6 @@ class MemorialTabs extends React.Component {
                   >
                      {this.state.showSettingsMenu ? <CloseIcon /> : <SettingsIcon/>}
                   </IconButton>
-                  <IconButton
-                     onClick={this.props.hideSecondaryAppHeader}
-                     style={{
-                        marginTop: 8,
-                        marginRight: 4,
-                     }}
-                  >
-                     <UnfoldLessIcon/>
-                  </IconButton>
                </div>
                <Collapse in={this.state.showSettingsMenu} style={{margin: 8}}>
                   <Typography variant="subheading">
@@ -74,6 +137,7 @@ class MemorialTabs extends React.Component {
                   indicatorColor="primary"
                   textColor="primary"
                   fullWidth
+                  centered
                >
                   <Tab label="Shrine" value="S" />
                   <Tab label="Chronicle" value="C" />
