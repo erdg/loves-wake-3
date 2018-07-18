2524dbbf9426c4bda541f847798c3e7612aa8fe7
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-07-18 16:03:53 -0500

    Remove unused things so react stops yelling at me

--- src/components/AppNav/AppMenu.js
+++ src/components/AppNav/AppMenu.js
@@ -1,12 +1,6 @@
 import React from 'react';
-// import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Drawer from '@material-ui/core/Drawer';
 import AppMenuItems from './AppMenuItems/';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import theme from 'theme';
 
 const AppMenu = (props) => {
    return (
