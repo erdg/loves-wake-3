d9c7bc510fa58ee0b783ecd27785171abc571104
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-09-08 13:59:24 -0500

    Replace annoying text with logo

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import LogoMultiColor from 'components/icons/LogoMultiColor';
 
 const Home = (props) => {
    return (
-      <div>
-         <Typography variant="display1">
-            THIS IS THE LOVE'S WAKE HOMEPAGE!!!
+      <div style={{margin: '40px auto', textAlign: 'center'}}>
+         <LogoMultiColor
+            height={160}
+            width={160}
+         />
+         <Typography variant="display1" style={{marginTop: 16}}>
+            LOVESWAKE
          </Typography>
       </div>
    )
