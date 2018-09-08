6225fbdb219fbc1a3e47c6d83b2353957e7fa3c9
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-09-08 14:01:57 -0500

    Add icon components

---
+++ src/components/icons/LogoMultiColor.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+const LogoMultiColor = (props) => (
+   <span {...props}
+      dangerouslySetInnerHTML={{__html:
+         `<svg
+            xmlns:dc="http://purl.org/dc/elements/1.1/"
+            xmlns:cc="http://creativecommons.org/ns#"
+            xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+            xmlns:svg="http://www.w3.org/2000/svg"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            id="svg3222"
+            version="1.1"
+            viewBox="0 0 13.229167 13.229167"
+            fill=${props.fill || ""}
+            height=${props.height || "50"}
+            width=${props.width ||"50"}>
+           <defs
+              id="defs3216">
+             <linearGradient
+                id="linearGradient837">
+               <stop
+                  style="stop-color:#fe9c7f;stop-opacity:1"
+                  offset="0"
+                  id="stop831" />
+               <stop
+                  id="stop841"
+                  offset="0.52432102"
+                  style="stop-color:#fe9c7f;stop-opacity:1" />
+               <stop
+                  id="stop839"
+                  offset="0.52432102"
+                  style="stop-color:#9b4e84;stop-opacity:1;" />
+               <stop
+                  id="stop833"
+                  offset="0.80462515"
+                  style="stop-color:#632b6c;stop-opacity:1" />
+               <stop
+                  style="stop-color:#280f36;stop-opacity:1"
+                  offset="1"
+                  id="stop835" />
+             </linearGradient>
+             <linearGradient
+                gradientTransform="matrix(13.775191,0,0,1.3540953,26.724451,-105.11236)"
+                gradientUnits="userSpaceOnUse"
+                y2="296.67603"
+                x2="-1.4654955"
+                y1="287.00375"
+                x1="-1.4654955"
+                id="linearGradient829"
+                xlink:href="#linearGradient837" />
+           </defs>
+           <metadata
+              id="metadata3219">
+             <rdf:RDF>
+               <cc:Work
+                  rdf:about="">
+                 <dc:format>image/svg+xml</dc:format>
+                 <dc:type
+                    rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+                 <dc:title></dc:title>
+               </cc:Work>
+             </rdf:RDF>
+           </metadata>
+           <g
+              transform="translate(0,-283.77082)"
+              id="layer1">
+             <circle
+                r="6.6145835"
+                cy="290.38541"
+                cx="6.6145835"
+                id="path847"
+                style="opacity:1;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.9879753;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+             <path
+                id="rect821"
+                d="m 6.6145834,284.11565 a 6.2697598,6.2697557 0 0 0 -6.26974715,6.26976 H 6.518718 12.88433 a 6.2697598,6.2697557 0 0 0 -6.2697466,-6.26976 z m -0.095866,6.26976 -0.4079534,0.35826 H 0.36302969 a 6.2697598,6.2697557 0 0 0 0.0265908,0.35828 H 5.7028115 l -0.4086533,0.35826 H 0.44350109 a 6.2697598,6.2697557 0 0 0 0.0741738,0.35826 H 4.8862051 L 4.477552,292.17676 H 0.60864214 a 6.2697598,6.2697557 0 0 0 0.12455531,0.35827 H 4.0695978 L 3.6616444,292.8933 H 0.87174691 a 6.2697598,6.2697557 0 0 0 0.17283789,0.35828 h 2.2084066 l -0.4079535,0.35826 H 1.2461124 a 6.2697598,6.2697557 0 0 0 0.2260189,0.35826 h 0.9649532 l -0.4079534,0.35829 H 1.7443335 a 6.2697598,6.2697557 0 0 0 4.8702499,2.32876 6.2697598,6.2697557 0 0 0 4.8730486,-2.32876 h -0.478628 l -0.408653,-0.35829 h 1.150387 a 6.2697598,6.2697557 0 0 0 0.233016,-0.35826 H 10.192397 L 9.7844439,293.25158 H 12.18948 a 6.2697598,6.2697557 0 0 0 0.163041,-0.35828 H 9.3764905 l -0.4086527,-0.35827 h 3.5309312 a 6.2697598,6.2697557 0 0 0 0.118956,-0.35827 H 8.5598852 l -0.4079539,-0.35829 h 4.5588597 a 6.2697598,6.2697557 0 0 0 0.08047,-0.35826 H 7.743278 l -0.4079533,-0.35826 h 5.5000233 a 6.2697598,6.2697557 0 0 0 0.03639,-0.35828 H 6.9273711 Z m 0.095866,-6.04304 0.098665,1.16718 0.024491,0.2918 0.02519,0.2925 0.024491,0.29179 c 0,0 0.00527,0.18452 0.069275,0.24842 0.063995,0.0638 0.2477109,0.0672 0.2477113,0.0672 l 0.2917951,0.0245 0.2910954,0.0245 0.2924948,0.0245 1.1671812,0.0979 -1.1671812,0.0994 -0.2924948,0.0246 -0.2910954,0.0245 -0.2917951,0.0246 c 0,0 -0.183734,0.006 -0.2477113,0.0672 -0.063994,0.064 -0.069275,0.24911 -0.069275,0.24911 l -0.024491,0.2918 -0.02519,0.2911 -0.024491,0.29249 -0.098665,1.16718 -0.098664,-1.16718 -0.024491,-0.29249 -0.02519,-0.2911 -0.024491,-0.2918 c 0,0 -0.00431,-0.18522 -0.067875,-0.24911 -0.064008,-0.0637 -0.2491106,-0.0672 -0.2491106,-0.0672 l -0.2917952,-0.0246 -0.2910953,-0.0245 -0.2924949,-0.0246 -1.1671804,-0.0987 1.1671804,-0.0987 0.2924949,-0.0245 0.2910953,-0.0245 0.2917952,-0.0245 c 0,0 0.185124,-0.006 0.2491106,-0.0672 0.063995,-0.064 0.067875,-0.24841 0.067875,-0.24841 l 0.024491,-0.29179 0.02519,-0.29251 0.024491,-0.2918 z"
+                style="opacity:1;fill:url(#linearGradient829);fill-opacity:1;stroke:none;stroke-width:1.14270985;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+           </g>
+         </svg>`
+      }}
+   />
+)
+
+export default LogoMultiColor;
---
+++ src/components/icons/ShrineIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ShrineIcon = (props) => (
+   <span {...props}
+      dangerouslySetInnerHTML={{__html:
+         `<svg xmlns="http://www.w3.org/2000/svg" id="svg3222" viewBox="0 0 5.2916665 5.2916669"
+         height="20" width="20">
+             <g transform="translate(0 -291.708)" id="layer1">
+                 <path id="path1139" transform="matrix(.26458 0 0 .26458 0 291.708)" d="M 10 0 L 9.375 1.375 L 8 2 L 9.375 2.625 L 10 4 L 10.625 2.625 L 12 2 L 10.625 1.375 L 10 0 z M 7 4 L 6 8 L 5 8 L 4 12 L 3 12 L 2 16 L 1 16 L 0 20 L 2 20 A 2.3121648 2.0000008 0 0 1 4 19 L 16 19 A 2.3121648 2.0000008 0 0 1 18 20 L 20 20 L 19 16 L 18 16 L 17 12 L 16 12 L 15 8 L 14 8 L 13 4 L 10.908203 4 L 10 5 L 9.0917969 4 L 7 4 z M 10 7 A 2.3121648 2.0000008 0 0 1 12 8 L 8 8 A 2.3121648 2.0000008 0 0 1 10 7 z M 8 11 L 12 11 A 2.3121648 2.0000008 0 0 1 14 12 L 6 12 A 2.3121648 2.0000008 0 0 1 8 11 z M 6 15 L 14 15 A 2.3121648 2.0000008 0 0 1 16 16 L 4 16 A 2.3121648 2.0000008 0 0 1 6 15 z"
+                 strokeWidth="0.831" strokeLinecap="round" />
+             </g>
+         </svg>`
+      }}
+   />
+)
+
+export default ShrineIcon;
