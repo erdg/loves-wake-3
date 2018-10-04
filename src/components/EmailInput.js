bd88454e9dbf31b0da07a6104d78a1bfdf17c74c
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-10-04 14:14:30 -0500

    Update inputs to 'outlined' style

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -1,29 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 
 
-const EmailInput = (props) => {
-   return (
-      <FormControl error={props.error ? true : false} aria-describedby="email-error">
-         <InputLabel htmlFor="em">Email</InputLabel>
-         <Input
-            id="em"
-            name="em"
-            value={props.em}
-            onChange={props.handleChange}
-         />
-         { props.error &&
-            <FormHelperText id="email-error">
-               Please enter a valid email
-            </FormHelperText>
-         }
-      </FormControl>
-   )
+class EmailInput extends React.Component {
+   componentDidMount () {
+      this.forceUpdate();
+   }
+   render () {
+      return (
+         <FormControl error={this.props.error ? true : false} aria-describedby="email-error" variant="outlined">
+            <InputLabel
+               htmlFor="em"
+               ref={ref => {
+                  this.labelRef = ReactDOM.findDOMNode(ref);
+               }}
+            > Email
+            </InputLabel>
+            <OutlinedInput
+               id="em"
+               name="em"
+               value={this.props.em}
+               onChange={this.props.handleChange}
+               labelWidth={this.labelRef ? this.labelRef.offsetWidth : 0}
+            />
+            { this.props.error &&
+               <FormHelperText id="email-error">
+                  Please enter a valid email
+               </FormHelperText>
+            }
+         </FormControl>
+      )
+   }
 }
 
 export default EmailInput;
--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
@@ -14,23 +15,31 @@ class PasswordInput extends React.Component {
       showPassword: false
    }
 
+   componentDidMount () {
+      this.forceUpdate();
+   }
+
    handleClick = () => {
       this.setState({ showPassword: !this.state.showPassword })
    }
 
    render () {
       return (
-         <FormControl>
+         <FormControl variant="outlined">
             <InputLabel
                htmlFor="pw"
+               ref={ref => {
+                  this.labelRef = ReactDOM.findDOMNode(ref);
+               }}
             > Password
             </InputLabel>
-            <Input
+            <OutlinedInput
                id="pw"
                name="pw"
                type={this.state.showPassword ? "text" : "password"}
                value={this.props.pw}
                onChange={this.props.handleChange}
+               labelWidth={this.labelRef ? this.labelRef.offsetWidth : 0}
                endAdornment={
                   <InputAdornment position="end">
                      <IconButton
