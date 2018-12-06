fd33411e30fe48f3f014a926ca7bb31d76d033a7
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-12-06 12:31:22 -0600

    Add 'Create Memorial' button, 'Confirm Account' modal

--- src/routes/User/index.js
+++ src/routes/User/index.js
@@ -3,21 +3,41 @@ import { connect } from 'unistore/react';
 import { actions } from 'store';
 import Typography from '@material-ui/core/Typography';
 import MemorialList from './MemorialList';
+import ConfirmAccountModal from './ConfirmAccountModal';
+import Button from '@material-ui/core/Button';
+import history from '../../history';
 
 class User extends React.Component {
+   state = {
+      showConfirmAccountModal: true
+   }
+
    componentDidMount () {
       !this.props.user.email && this.props.getUserData();
+   }
+
+   closeConfirmAccountModal = () => {
+      this.setState({ showConfirmAccountModal: false })
    }
 
    render () {
       return (
          <div>
+            <ConfirmAccountModal
+               open={this.props.user.confirmed && this.props.user.confirmed !== 'true' ? true : false}
+               closeConfirmAccountModal={this.closeConfirmAccountModal}
+            />
             <Typography variant="display1">
                User Profile Page
             </Typography>
             <Typography variant="body1">
                Logged in with email: {this.props.user.email}
             </Typography>
+            <Button
+               onClick={() => history.push("/create-memorial")}
+               variant="outlined"
+            > Create New Memorial
+            </Button>
             <MemorialList memorials={this.props.user.memorials || []} />
          </div>
       )
