33ac161d0fd70e4b9991a1bb5030d4b954d9dd87
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-12-06 12:35:08 -0600

    Add placeholder text, if no memorials yet

--- src/routes/User/MemorialList.js
+++ src/routes/User/MemorialList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import history from '../../history';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import MemorialAvatar from 'components/MemorialAvatar';
@@ -10,17 +11,29 @@ const MemorialList = (props) => (
       <Typography variant="title">
          Memorials
       </Typography>
-      <List>
-         {props.memorials.map((m, i) => (
-            <ListItem key={i}
-               dense
-               button
-               onClick={() => history.push("/memorial/" + m.urlStr + "/" + m.urlNm)}
-            >
-               <MemorialAvatar src={m.avatar} name={m.nm} variant="subheading" />
-            </ListItem>
-         ))}
-      </List>
+      {props.memorials.length === 0 ?
+         <div>
+            <Typography variant="body1">
+               You have not created a memorial yet.
+            </Typography>
+            <Button
+               onClick={() => history.push("/create-memorial")}
+            > Create a memorial
+            </Button>
+         </div>
+            :
+         <List>
+            {props.memorials.map((m, i) => (
+               <ListItem key={i}
+                  dense
+                  button
+                  onClick={() => history.push("/memorial/" + m.urlStr + "/" + m.urlNm)}
+               >
+                  <MemorialAvatar src={m.avatar} name={m.nm} variant="subheading" />
+               </ListItem>
+            ))}
+         </List>
+      }
    </div>
 )
 
