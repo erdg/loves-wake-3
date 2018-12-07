d93b89189f12604da5e87501f2380f1fec328888
Erik Gustafson <erik.d.gustafson@gmail.com>
2018-12-07 09:36:53 -0600

    Add placeholder text empty state

--- src/routes/Limbo/LimboContentList.js
+++ src/routes/Limbo/LimboContentList.js
@@ -14,81 +14,102 @@ class LimboContentList extends React.Component {
    render () {
       // only chronicle items that have been 'published'
       // let unpublishedItems = this.props.memorial.items.filter(itm => itm.published === "false");
-      return (
-         <div>
-            {this.props.memorial.items.map((item, i) => (
-               <LazyLoad 
-                  key={i}
-                  height={200}
-                  offset={600}
-                  once
-               >
-                  {item.published === "false" ?
-                  <UnpublishedCard
-                     item={item}
-                     memorialId={this.props.memorial.id}
-                     born={this.props.memorial.born.split(/[^\d]/).find((n) => n.length === 4)}
-                     died={this.props.memorial.died.split(/[^\d]/).find((n) => n.length === 4)}
-                  />
-                        :
-                  <div style={{display: 'flex'}}>
-                     <Card
-                        style={{width: '100%'}}
-                        elevation={0}
-                     >
-                        <div
-                           style={{
-                              display: 'flex',
-                              alignItems: 'center',
-                              justifyContent: 'space-between',
-                              width: '100%',
-                           }}
+
+      if (this.props.memorial.items.length === 0) {
+         return (
+            <div style={{width: '92%', margin: '0 auto', marginTop: 16}}>
+               <Typography variant="headline" paragraph >
+                  This is Limbo.
+               </Typography>
+               <Typography variant="subheading" paragraph >
+                  It's a collaborative workspace. All uploaded content lands here first. You and your
+                  fellow contributors can work together here to chase down dates and stories. When an
+                  item is ready, it can be published to Chronicle.
+               </Typography>
+               <Typography variant="subheading" paragraph >
+                  Begin by clicking the "add" icon above. You can upload photos, videos, audio, or 
+                  written notes. Fill in any details when uploading if you'd like, but only a file 
+                  or story is needed to submit.
+               </Typography>
+            </div>
+         )
+      } else {
+         return (
+            <div>
+               {this.props.memorial.items.map((item, i) => (
+                  <LazyLoad 
+                     key={i}
+                     height={200}
+                     offset={600}
+                     once
+                  >
+                     {item.published === "false" ?
+                     <UnpublishedCard
+                        item={item}
+                        memorialId={this.props.memorial.id}
+                        born={this.props.memorial.born.split(/[^\d]/).find((n) => n.length === 4)}
+                        died={this.props.memorial.died.split(/[^\d]/).find((n) => n.length === 4)}
+                     />
+                           :
+                     <div style={{display: 'flex'}}>
+                        <Card
+                           style={{width: '100%'}}
+                           elevation={0}
                         >
-                           {item.imageSrc &&
-                              <CardMedia image={item.imageSrc}
-                                 style={{
-                                    height: 80,
-                                    width: 80,
-                                    marginTop: 16,
-                                    marginBottom: 16
-                                 }}
-                              />
-                           }
-                           <CardContent
+                           <div
                               style={{
-                                 padding: '0px 16px',
-                                 width: '100%'
+                                 display: 'flex',
+                                 alignItems: 'center',
+                                 justifyContent: 'space-between',
+                                 width: '100%',
                               }}
                            >
-                              <div
+                              {item.imageSrc &&
+                                 <CardMedia image={item.imageSrc}
+                                    style={{
+                                       height: 80,
+                                       width: 80,
+                                       marginTop: 16,
+                                       marginBottom: 16
+                                    }}
+                                 />
+                              }
+                              <CardContent
                                  style={{
-                                    display: 'flex',
-                                    alignItems: 'center',
+                                    padding: '0px 16px',
+                                    width: '100%'
                                  }}
                               >
-                                 <Typography variant="caption">
-                                    Card Published to Chronicle
-                                 </Typography>
-                                 <Button
-                                    onClick={() => this.props.unpublishChronicleItem(item.id, this.props.memorial.id)}
+                                 <div
                                     style={{
-                                       marginLeft: 'auto',
-                                       marginTop: 8,
-                                       marginBottom: 8
+                                       display: 'flex',
+                                       alignItems: 'center',
                                     }}
-                                 > Unpublish
-                                 </Button>
-                              </div>
-                           </CardContent>
-                        </div>
-                        <Divider />
-                     </Card>
-                  </div>
-                  }
-               </LazyLoad>
-            ))}
-         </div>
-      )
+                                 >
+                                    <Typography variant="caption">
+                                       Card Published to Chronicle
+                                    </Typography>
+                                    <Button
+                                       onClick={() => this.props.unpublishChronicleItem(item.id, this.props.memorial.id)}
+                                       style={{
+                                          marginLeft: 'auto',
+                                          marginTop: 8,
+                                          marginBottom: 8
+                                       }}
+                                    > Unpublish
+                                    </Button>
+                                 </div>
+                              </CardContent>
+                           </div>
+                           <Divider />
+                        </Card>
+                     </div>
+                     }
+                  </LazyLoad>
+               ))}
+            </div>
+         )
+      }
    }
 }
 
